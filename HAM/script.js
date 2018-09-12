73917b648124fb0171ee06fcc1ec4a9f20ea9b98
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-09-12 21:50:17 +0300

    Filter is (sort of) working

--- HAM/script.js
+++ HAM/script.js
@@ -7,3 +7,20 @@ $('.services__menu-item').click(function () {
     $('.services__item').removeClass('services__item_active');
     $($('.services__item')[num]).addClass('services__item_active');
 });
+
+
+$('.aw__nav-item').click(function () {
+
+    $(".grid-item").hide();
+    let type = ($(this).attr("data-info"));
+    if(type === "all"){
+        $(".grid-item").slice(0, 12).show();
+    } else{
+        $(".grid-item").show();
+        $(".grid-item").not(`.${type}`).addClass("passive");
+        $(".grid-item").not(`.${type}`).hide();
+        $(".grid-item").is(`.${type}`).slice(0, 12).addClass("active");
+        $(".grid-item").is(`.${type}`).slice(0, 12).show();
+    }
+
+})
