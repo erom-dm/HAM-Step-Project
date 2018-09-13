04e50f5f7d8998e6d9eaf87f0207c3a37fb8bd8a
Barkov.Ilia <barkov.ilia@gmail.com>
2018-09-13 23:46:57 +0300

    added reviews block with slider

--- HAM/script.js
+++ HAM/script.js
@@ -17,4 +17,108 @@ $(document).ready(function () {
             $('.modal').removeClass('active');
         }
     });
+    //---------------------slider----------------------------
+    let index = 1;
+    let measure = 4;
+    selectCurrentSmall();
+
+    let moveNext = function(counter) {
+        if (counter > 0) {
+            let items = $('.reviews__item');
+            let width = items.parent().width();
+            let current = $($(items[0]));
+            current.animate({marginLeft: `-${width}px`}, 1000/counter, 'linear', function () {
+                current.css({'margin':'0'});
+                $('.reviews__slider ul').append(current);
+                moveNext(counter-1);
+            });
+        }
+    };
+
+    let movePrev = function(counter) {
+        if (counter > 0) {
+            let items = $('.reviews__item');
+            let width = items.parent().width();
+            let current = $($(items[items.length-1])).css({'margin-left':`-${width}px`});
+
+            $('.reviews__slider ul').prepend(current);
+            current.animate({marginLeft: `0`}, 1000/counter ,'linear', function () {
+                movePrev(counter-1);
+            });
+        }
+    };
+
+    $(document).on('click', '#sliderNext', function (e) {
+        e.preventDefault();
+        moveNext(1);
+        deleteSelectCurrentSmall();
+        index++;
+        if (index > measure) {
+            index = 1;
+            let smallItems = $('.reviews__item-img.small');
+            for (let i = 0; i < 4; i++) {
+                $(smallItems[i]).removeClass('active');
+                $('.reviews__mini-slider').append(smallItems[i]);
+            }
+
+            smallItems = $('.reviews__item-img.small');
+            for (let i = 0; i < 4; i++) {
+                $(smallItems[i]).addClass('active');
+            }
+        }
+        selectCurrentSmall();
+    });
+
+    $(document).on('click', '#sliderPrev', function (e) {
+        e.preventDefault();
+        movePrev(1);
+        deleteSelectCurrentSmall();
+        index--;
+        if (index < 1) {
+            index = measure;
+            let smallItems = $('.reviews__item-img.small');
+            for (let i = smallItems.length-1; i > smallItems.length-1 - measure; i--) {
+                $(smallItems[i]).addClass('active');
+                $('.reviews__mini-slider').prepend(smallItems[i]);
+            }
+
+            smallItems = $('.reviews__item-img.small');
+            for (let i = smallItems.length-1; i > smallItems.length-1 - measure; i--) {
+                $(smallItems[i]).removeClass('active');
+            }
+        }
+        selectCurrentSmall();
+    });
+
+    $(document).on('click', '.reviews__item-img.small', function (e) {
+        e.preventDefault();
+        deleteSelectCurrentSmall();
+        let attr = $(this).attr('data-info');
+        attr = attr>4? attr%4: attr;
+
+        if (attr > index) {
+            moveNext(attr-index);
+            index = attr;
+        } else if(attr < index) {
+            movePrev(index-attr);
+            index = attr;
+        }
+        selectCurrentSmall();
+
+    });
+
+
+    function selectCurrentSmall() {
+        $($('.reviews__item-img.small.active')[index-1]).css({
+            'border-color':'#18cfab',
+            'align-self': 'baseline'
+        });
+    }
+
+    function deleteSelectCurrentSmall() {
+        $($('.reviews__item-img.small.active')[index-1]).css({
+            'border-color':'rgba(31, 218, 181, 0.2)',
+            'align-self': 'auto'
+        });
+    }
 });
