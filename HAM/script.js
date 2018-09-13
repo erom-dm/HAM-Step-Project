39cdc3e58423e85955c50fb6c249e6c1091d20f5
Barkov.Ilia <barkov.ilia@gmail.com>
2018-09-13 23:20:15 +0300

    added js effects for modal window

--- HAM/script.js
+++ HAM/script.js
@@ -1,7 +1,20 @@
-$( document ).ready(function() {
-    $(document).on('click','.services__menu-item',function(){
+$(document).ready(function () {
+    // ---------------services-tab------------------------
+    $(document).on('click', '.services__menu-item', function () {
         $('.services__menu-item').removeClass('active');
         $(this).addClass('active');
         $('.services__item').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    //---------------modal-window------------------------
+    $(document).on('click', '.purchase-block__button', function () {
+        $('.modal__wrapper').addClass('active');
+        $('.modal').addClass('active');
+    });
+
+    $(document).on('click', '.modal__wrapper, .modal__social-button, .register__button', function (e) {
+        if (e.target === this) {
+            $('.modal__wrapper').removeClass('active');
+            $('.modal').removeClass('active');
+        }
     });
 });
