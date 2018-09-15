00d19dd82d0254ec9f91697a8c9be8b435bd1074
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-09-15 15:20:58 +0300

    soft scroll implemented

--- HAM/script.js
+++ HAM/script.js
@@ -4,60 +4,65 @@ $( document ).ready(function() {
         $(this).addClass('active');
         $('.services__item').removeClass('active').eq($(this).index()).addClass('active');
     });
-});
 
-$(function () {
-    let maxItemCount = 12;
-    let type = "all";
-    let item = $(".grid-item");
-    item.slice(0, maxItemCount).show();
-    $('.aw__nav-item').click(function (e) {
+    let link = $('.nav-buttons');
+    link.click(function (e) {
         e.preventDefault();
-        item.hide();
-        type = ($(this).attr("data-info"));
-        if(type === "all"){
-            item.slice(0, maxItemCount).show();
-        } else{
-            item.show();
-            item.not(`.${type}`).hide();
-            item.filter(`.${type}`).slice(maxItemCount).hide();
-        }
-
-    });
-    $(".load_more-btn").click(function(e){
-        e.preventDefault();
-        if (maxItemCount < 36){
-            maxItemCount += 12;
-        }
-        if(type === "all"){
-            item.slice(0, maxItemCount).show();
-        } else {
-            item.filter(`.${type}`).slice(0, maxItemCount).show();
-        }
-
-        if (maxItemCount === 36){
-            $(".load_more-btn").fadeOut();
-        }
+        $('body, html').animate({
+            scrollTop: $($(this).children()[0].hash).offset().top,
+        },1000);
     });
 
+    $(function () {
+        let maxItemCount = 12;
+        let type = "all";
+        let item = $(".grid-item");
+        item.slice(0, maxItemCount).show();
+        $('.aw__nav-item').click(function (e) {
+            e.preventDefault();
+            item.hide();
+            type = ($(this).attr("data-info"));
+            if(type === "all"){
+                item.slice(0, maxItemCount).show();
+            } else{
+                item.show();
+                item.not(`.${type}`).hide();
+                item.filter(`.${type}`).slice(maxItemCount).hide();
+            }
+
+        });
+        $(".load_more-btn").click(function(e){
+            e.preventDefault();
+            if (maxItemCount < 36){
+                maxItemCount += 12;
+            }
+            if(type === "all"){
+                item.slice(0, maxItemCount).show();
+            } else {
+                item.filter(`.${type}`).slice(0, maxItemCount).show();
+            }
+
+            if (maxItemCount === 36){
+                $(".load_more-btn").fadeOut();
+            }
+        });
+
+    });
+
+    let $grid = $('.masonry').masonry({
+        itemSelector: '.item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer',
+        gutter: 15
+    });
+
+    $grid.imagesLoaded().progress( function() {
+        $grid.masonry();
+    });
+
+    $('.gallery-btn').on( 'click', function() {
+        $('.item.add').toggleClass('add');
+        $grid.masonry();
+    });
 });
 
-let $grid = $('.masonry').masonry({
-    itemSelector: '.item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer',
-    gutter: 15
-});
-
-$grid.imagesLoaded().progress( function() {
-    $grid.masonry();
-});
-
-$('.gallery-btn').on( 'click', function() {
-    $('.item.add').toggleClass('add');
-    $grid.masonry();
-});
-
-
-
-// create <div class="grid-item"></div>
