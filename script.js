9b0bd41b40d81edb63c49c33f15327e302c7cf28
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-10-04 19:49:51 +0300

    added modal window to header purchase button

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ $(document).ready(function () {
         $('.modal').addClass('active');
     });
 
+    $(document).on('click', '.header_purchase-btn', function () {
+        $('.modal__wrapper').addClass('active');
+        $('.modal').addClass('active');
+    });
+
     $(document).on('click', '.modal__wrapper, .modal__social-button, .register__button', function (e) {
         if (e.target === this) {
             $('.modal__wrapper').removeClass('active');
